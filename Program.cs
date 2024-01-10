04910dfdb62a6c8f37e3714e346d0735310259a3
OmiSeerDev <omarmtinajero@gmail.com>
2024-01-09 18:14:01 -0600

    First commit on C#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program {
+    private static void Main(string[] args) {
+            Console.WriteLine("Area Calculator");
+            double sideA;
+            double sideB;
+
+            Console.WriteLine("Type a width for the rectangle in cm");
+            sideA = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Type a height for the rectangle in cm");
+            sideB = Convert.ToDouble(Console.ReadLine());
+
+        var result = sideA * sideB;
+        Console.WriteLine("The area for this rectangle equals:" + result);
+    }
+}
